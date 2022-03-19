5f46fece00643a57dbe417d2bdd115443cc54794
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2022-03-19 20:31:01 +0530

    added forms need to design waves and animation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,27 +25,33 @@ function Form() {
     <div className="font-Mons bg-grad w-full text-white flex justify-center items-center py-20 px-5">
       <form
         onSubmit={submitHandler}
-        className="bg-white w-full text-black px-6 pt-4 pb-10  rounded-2xl max-w-xl sixTen:pt-4 sixTen:pb-10  sixTen:px-16 relative"
+        className="bg-white w-full text-black px-6 pt-4 pb-10  max-w-xl sixTen:pt-4 sixTen:pb-10  sixTen:px-16 relative  border-none"
       >
-        <div className="absolute -top-12 -right-7 z-10">
-          {/* <svg>
+        <div className="absolute -top-5 right-0 z-10 sixTen:right-8">
+          <svg
+            width="120"
+            height="80"
+            viewBox="0 0 120 80"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
-              d="M58.7763 66.3158L199.999 0L82.8875 78.9474L151.777 120L199.999 0L0 37.8947L58.7763 66.3158Z"
+              d="M29.3881 33.1579L99.9995 0L41.4437 39.4736L75.8883 59.9999L99.9995 0L0 18.9473L29.3881 33.1579Z"
               fill="#7383BF"
             />
             <path
-              d="M82.8886 78.9474L69.3277 120L58.7773 66.3158L200 0L82.8886 78.9474Z"
+              d="M41.4437 39.4736L34.6638 59.9999L29.3881 33.1579L99.9995 0L41.4437 39.4736Z"
               fill="#556080"
             />
             <path
-              d="M82.8872 78.9475L69.1094 120L109.919 95.0559L82.8872 78.9475Z"
+              d="M41.4437 39.4736L34.5547 60L54.9596 47.5279L41.4437 39.4736Z"
               fill="#464F66"
             />
-          </svg> */}
+          </svg>
         </div>
-        <h1 className="text-xl my-5 font-bold bg-green-500">Register !</h1>
+        <h1 className="text-xl my-5 font-bold">Register !</h1>
 
-        <div className="w-full mb-6 rounded-2xl bg-gray-50 px-4 ring-2 ring-gray-200">
+        <div className="w-full mb-6 rounded-md bg-gray-50 px-4 ring-2 ring-gray-200">
           <input
             ref={nameRef}
             value={setName}
@@ -99,17 +105,23 @@ function Form() {
             className="my-3 w-full border-none bg-transparent outline-none focus:outline-none"
           />
         </div>
-        <button className="w-full border-2 border-transparent mb-6 rounded-2xl bg-[#176698] py-3 font-bold text-white hover:border-[#e5e7eb] hover:border-2 hover:bg-white hover:text-[#176698] transition-colors">
+        <button className="w-full border-2 border-transparent mb-6 rounded-2xl bg-[#176698] py-3 font-bold text-white hover:border-[#176698] hover:border-2 hover:bg-white hover:text-[# #e5e7eb] transition-colors">
           Submit
         </button>
-        <div className="absolute bottom-0 z-10">
-          {/* <svg>
+        <div className="absolute bottom-0 left-0 w-full -z-2">
+          <svg
+            // width="677"
+            // height="197"
+            viewBox="0 0 677 197"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M676 73.1658C555.591 127.759 101.5 98.0251 1 2V196H676V73.1658Z"
               fill="#176698"
               stroke="#176698"
             />
-          </svg> */}
+          </svg>
         </div>
       </form>
     </div>
