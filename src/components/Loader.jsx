c8c8096cb05062e4cbf2abd32e12f06f02e9dd4d
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2022-03-23 19:45:01 +0530

    Added Loading State2

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { FillingBottle } from "react-cssfx-loading/lib";
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex items-center justify-center h-screen w-screen bg-[#F5FEFD]">
       <FillingBottle color="#176698" width="100px" height="100px" />
     </div>
   );
