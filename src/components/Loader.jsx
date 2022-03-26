ac842d1ca27be880b01fd9ee6c5e07efe89f71a3
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2022-03-27 00:34:24 +0530

    fixed bugs

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { FillingBottle } from "react-cssfx-loading/lib";
+import { BouncingBalls } from "react-cssfx-loading/lib";
 
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-[#F5FEFD]">
-      <FillingBottle color="#176698" width="100px" height="100px" />
+      <BouncingBalls color="#176698" width="50px" height="50px" />
     </div>
   );
 };
