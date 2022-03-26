b7dd8bd5fcdfcbea8906056d631a903e8a2458f2
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2022-03-27 00:41:10 +0530

    bug fixes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { BouncingBalls } from "react-cssfx-loading/lib";
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-[#F5FEFD]">
-      <BouncingBalls color="#176698" width="20px" height="20px" />
+      <BouncingBalls color="#176698" width="30px" height="30px" />
     </div>
   );
 };
